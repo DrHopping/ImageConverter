e786ee7fe08ffd4fc6446e6bf0a684ea9de31291
Mykyta Pedorenko <mykyta_pedorenko@epam.com>
2021-03-21 23:03:33 +0200

    Update BmpReader.cs

--- ImageConverter/Readers/BmpReader.cs
+++ ImageConverter/Readers/BmpReader.cs
@@ -9,10 +9,11 @@ namespace ImageConverter.Readers
 
     public class BmpReader : IReader
     {
+        private const int HeaderSize = 138;
         public Image Read(FileStream stream)
         {
             using var reader = new BinaryReader(stream);
-            var info = reader.ReadBytes(54);
+            var info = reader.ReadBytes(HeaderSize);
             var width = info[18] | info[19] << 8 | info[20] << 16 | info[21] << 24;
             var height = info[22] | info[23] << 8 | info[24] << 16 | info[25] << 24;
             var fileSize = info[2] | info[3] << 8 | info[4] << 16 | info[5] << 24;
@@ -23,7 +24,7 @@ namespace ImageConverter.Readers
         {
             var image = new Image (height, width);
             var skipAmount = ((3 * width) % 4 == 0) ? 0 : ((4 - (3 * width) % 4));
-            for (int i = 0; i < height; i++)
+            for (int i = height - 1; i >= 0; i--)
             {
                 for (int j = 0; j < width; j++)
                 {
@@ -33,8 +34,9 @@ namespace ImageConverter.Readers
                         Green = reader.ReadByte(),
                         Red = reader.ReadByte()
                     };
+                    reader.ReadByte();
                 }
-                reader.ReadBytes(skipAmount);
+                //reader.ReadBytes(skipAmount);
             }
             return image;
         }
